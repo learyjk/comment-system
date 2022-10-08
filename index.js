c9beddfb65c5eec85dc9658b64f5cf40f2966517
Keegan Leary <leary.keegan@gmail.com>
2022-10-08 13:13:27 -0700

    hello world

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("test");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/hello", (req, res) => {
+  res.send("Hello There");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
