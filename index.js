48d5267495411d0fb44c4e27297ea7bdd2b5e29e
Keegan Leary <leary.keegan@gmail.com>
2022-10-08 13:24:47 -0700

    /

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  console.log("get request to /");
+  res.send("Hello There");
+});
+
 app.get("/hello", (req, res) => {
   res.send("Hello There");
 });
